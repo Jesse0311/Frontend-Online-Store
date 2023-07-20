665b57040ed1a5ed1999353e36093c68cff7e8fd
Jesse0311 <j.christians@lab.clicklearning.org>
2023-07-20 18:19:48 +0200

    Added Gallery Page
    this page is for inspiration

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,29 @@ const sizeCheckbox = document.querySelector('.size-checkbox');
 $('.size-checkbox').on('change', function(){
     $('.size-checkbox').not(this).prop('checked', false);
 });
+
+// Swiper script
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    breakpoints: {
+      450: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      820: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+    },
+  });
