ba3f31092642a53f27dbdffcfe0531cd7eb28814
Jesse0311 <j.christians@lab.clicklearning.org>
2023-07-24 08:30:18 +0200

    Added social icons in the footer

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 document.querySelector('#username').innerHTML = localStorage.getItem('userName');
 
+
 var logoutButton = document.querySelector("#logout-button");
 
 if (localStorage.getItem('userName') === null) {
@@ -27,31 +28,7 @@ $('.size-checkbox').on('change', function () {
     $('.size-checkbox').not(this).prop('checked', false);
 });
 
-// Swiper script //
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints: {
-        450: {
-            slidesPerView: 2,
-            spaceBetween: 0,
-        },
-        820: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 0,
-        },
-    },
-});
 
 
 
